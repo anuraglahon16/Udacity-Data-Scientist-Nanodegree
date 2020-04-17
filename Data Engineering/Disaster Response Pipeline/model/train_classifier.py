65cdac3c3c797841e5774c4afc0e2ab865db7bd5
Anurag <anuraglahondp@gmail.com>
2020-04-16 22:44:39 -0600

    Add files via upload

---
+++ Data Engineering/Disaster Response Pipeline/model/train_classifier.py
@@ -0,0 +1,170 @@
+
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+nltk.download('stopwords')
+import pickle
+import re
+import os
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
+import warnings
+import time
+warnings.filterwarnings('ignore')
+
+
+
+def load_data(database_filepath):
+    '''
+    Load data from database as dataframe
+    Input:
+            database_filepath : File path of sql database
+    Output:
+        X : Message data (features)
+        Y : Categories (target)
+        category_names : Labels for 36 categories
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMessages',con=engine)
+    X=df['message']
+    Y=df.iloc[:,4:]
+    category_names = list(df.columns[4:])
+    return X,Y,category_names
+
+
+def tokenize(text):
+    '''
+    Tokenize and clean text
+    Input:
+    text:  message text
+    Output : 
+    lem : Tokenized,cleaned and lemmatized text 
+    '''
+    text=re.sub(r"[^a-zA-Z0-9]"," ",text.lower())
+    
+    #Tokenize
+    words=word_tokenize(text)
+    
+    #Remove stop words
+    StopWords=stopwords.words('english')
+    words=[word for word in words if word not in StopWords]
+    
+    #Lemmatize
+    lemmatizer=WordNetLemmatizer()
+    lem = [lemmatizer.lemmatize(w,pos='n').strip() for w in words]
+    lem = [lemmatizer.lemmatize(w,pos='v').strip() for w in words]
+    
+    return lem
+
+
+def build_model():
+    '''
+    Build model with GridSearchCV
+    
+    Input:None
+    Output:
+            Results after performing GridSearchCV
+    '''
+    
+    
+    pipeline = Pipeline([
+
+                        ('vect', CountVectorizer(tokenizer=tokenize)),
+
+                        ('tfidf', TfidfTransformer()),
+
+                        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+
+                        ])
+
+
+
+    parameters = {'clf__estimator__n_estimators': [5],
+
+                  'clf__estimator__min_samples_split': [2, 3, 4],
+
+                  'clf__estimator__criterion': ['entropy', 'gini']
+
+                 }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    
+
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    Input:
+    model: trained model
+    X_test: Test features
+    Y_test: Test targets
+    category_names : Target labels
+    '''
+    #predict
+    y_pred = model.predict(X_test)
+    
+    #print classification report
+    for i in range(len(category_names)):
+
+        print("Category:", category_names[i],"\n", classification_report(Y_test.iloc[:, i].values, y_pred[:, i]))
+
+        print('Accuracy of %25s: %.2f' %(category_names[i], accuracy_score(Y_test.iloc[:, i].values, y_pred[:,i])))
+
+
+
+
+def save_model(model, model_filepath):
+    '''
+    Save model as pickle file
+    Input:
+    
+    model: model to be saved
+    model_filepath : path of the output pick file
+    
+    Output: A pickle file of saved model
+    '''
+    pickle.dump(model,open(model_filepath,'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
